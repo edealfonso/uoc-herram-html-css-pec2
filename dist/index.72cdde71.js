0b0a55c3e65bbc77d8504ce793de0d9ae4667612
elsa.de.alfonso <elsa.de.alfonso@gmail.com>
2022-04-10 01:44:55 +0200

    Initial commit 3

--- dist/index.72cdde71.js
+++
@@ -1,2 +0,0 @@
-document.body.style.backgroundColor="#239412";console.log("Hello world");
-//# sourceMappingURL=index.72cdde71.js.map
--- dist/index.77880129.js
+++
@@ -1,2 +0,0 @@
-!function(){document.body.style.backgroundColor="#239412";console.log("Hello ".concat("world"))}();
-//# sourceMappingURL=index.77880129.js.map
