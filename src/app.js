b54fee82adb14b7a89abeff39c144e96c9e23ecb
elsa.de.alfonso <elsa.de.alfonso@gmail.com>
2022-04-11 17:54:15 +0200

    v beta pagina

--- src/app.js
+++ src/app.js
@@ -1,4 +1,42 @@
-document.body.style.backgroundColor = '#239412';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
-const name = 'world';
-console.log(`Hello ${name}`);
+document.addEventListener('DOMContentLoaded', function () {
+
+    // const paragraphs = document.getElementsByTagName('p');
+    const items = document.querySelectorAll('main > *:not(.volver)');
+    [].forEach.call(items, (item) => { item.setAttribute('data-aos','fade-in') })
+
+    setTimeout(() => {
+        AOS.init({
+            duration: 500,
+            once: true
+        });
+    }, 300);
+    
+
+}, false);
+
+
+// jaula
+
+let jaula = document.getElementById('cage');
+let titulo = document.getElementById('title');
+
+function updateDisplay(e) {
+    titulo.style.left = `${e.pageX - jaula.offsetLeft - titulo.offsetWidth/2 }px`;
+    titulo.style.top = `${e.pageY - jaula.offsetTop - titulo.offsetHeight/2}px`;
+}
+
+function centerBox(event) {
+    titulo.style.left = '50%';
+    titulo.style.top ='50%';
+}
+
+if (jaula) {
+
+    jaula.addEventListener( 'mousemove', (e) => { updateDisplay(e) } );
+    jaula.addEventListener( 'mouseenter', (e) => { updateDisplay(e) } );
+    jaula.addEventListener( 'mouseleave', (e) => { centerBox(e) } );
+    
+}
