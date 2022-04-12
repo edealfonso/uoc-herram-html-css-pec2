e885283656e602e05e5af9e345cb2b1eaf7d8dad
elsa.de.alfonso <elsa.de.alfonso@gmail.com>
2022-04-12 11:58:39 +0200

    sass

--- src/app.js
+++ src/app.js
@@ -1,31 +1,19 @@
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
-document.addEventListener('DOMContentLoaded', function () {
-
-    // const paragraphs = document.getElementsByTagName('p');
-    const items = document.querySelectorAll('main > *:not(.volver)');
-    [].forEach.call(items, (item) => { item.setAttribute('data-aos','fade-in') })
-
-    setTimeout(() => {
-        AOS.init({
-            duration: 500,
-            once: true
-        });
-    }, 300);
-    
-
-}, false);
+// hide body initially
+document.body.classList.add('hidden');
 
 
-// jaula
+// jaula animation
+// ---------------
 
 let jaula = document.getElementById('cage');
 let titulo = document.getElementById('title');
 
 function updateDisplay(e) {
-    titulo.style.left = `${e.pageX - jaula.offsetLeft - titulo.offsetWidth/2 }px`;
-    titulo.style.top = `${e.pageY - jaula.offsetTop - titulo.offsetHeight/2}px`;
+    titulo.style.left = `${e.pageX - jaula.offsetLeft }px`;
+    titulo.style.top = `${e.pageY - jaula.offsetTop }px`;
 }
 
 function centerBox(event) {
@@ -34,9 +22,48 @@ function centerBox(event) {
 }
 
 if (jaula) {
-
     jaula.addEventListener( 'mousemove', (e) => { updateDisplay(e) } );
     jaula.addEventListener( 'mouseenter', (e) => { updateDisplay(e) } );
-    jaula.addEventListener( 'mouseleave', (e) => { centerBox(e) } );
-    
+    jaula.addEventListener( 'mouseleave', (e) => { centerBox(e) } ); 
 }
+
+
+// on document ready
+// -----------------
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    // AOS prepare
+
+    // childs of main that are not secitons / articles
+    const items = document.querySelectorAll('main > *:not(.volver):not(section):not(article)');
+    [].forEach.call(items, (item) => { item.setAttribute('data-aos','fade-in') })
+
+    // childs of sections 
+    const items2 = document.querySelectorAll('section > *:not(.volver)');
+    [].forEach.call(items2, (item) => { item.setAttribute('data-aos','fade-in') })
+  
+    // childs of articles 
+    const items3 = document.querySelectorAll('article > *:not(.volver)');
+    [].forEach.call(items3, (item) => { item.setAttribute('data-aos','fade-in') })
+      
+}, false);
+
+
+// on window ready
+// -----------------
+
+window.onload = (event) => {
+
+    // unhide on window load
+    document.body.classList.remove('hidden');
+
+    // AOS load
+    setTimeout(() => {
+        AOS.init({
+            duration: 500,
+            once: true
+        });
+    }, 100);
+
+};
